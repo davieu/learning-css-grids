1a84d52b05ec1b38bf8a85e824af223af9258a2f
davieu <davisumana@gmail.com>
2019-08-17 03:16:56 -0400

    messing with nav bar

--- nav-bar/main.js
+++ nav-bar/main.js
@@ -1,26 +1,33 @@
+let toggled = false;
 const navSlide = () => {
   const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
+  // const nav = document.querySelector('.nav-links');
   const navLinks = document.querySelectorAll('.nav-links li');
+  const linksWrapper = document.querySelector('.links-wrapper');
 
+
+  // linksWrapper.style.transition = 'width 0.5s ease-in';
+  // linksWrapper.style.transition = 'none';
+  // linksWrapper.style.transition = 'none';
 
   burger.addEventListener('click', () => {
     // Toggle Nav
-    nav.classList.toggle('nav-active');
+    
+    // linksWrapper.style.transition = 'width 0.5s'; 
+    linksWrapper.classList.toggle('nav-active');
 
     // Animate Links
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = '';
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + .4}s`
+        link.style.animation = `navLinkFade 0.3s ease forwards ${index /7 + .3}s`
       }
     });
     // Burger Animation
-    burger.classList.toggle('toggle');
+    burger.classList.toggle('toggle')
   });
-
-
+  // linksWrapper.style.transition = 'none';
 }
 
 const app = () => {

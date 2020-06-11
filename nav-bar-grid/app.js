a48d91e5dfda3b70951eb524b86894a2267ec8c8
davieu <davisumana@gmail.com>
2020-06-10 22:32:11 -0400

    worked on flex/grid fake trucking site

--- nav-bar-grid/app.js
+++ nav-bar-grid/app.js
@@ -7,20 +7,21 @@ const navSlide = () => {
   const linksWrapper = document.querySelector('.links-wrapper');
   const sideBar = document.querySelector('.side-bar');
 
-
   // linksWrapper.style.transition = 'width 0.5s ease-in';
   // linksWrapper.style.transition = 'none';
   // linksWrapper.style.transition = 'none';
 
   burger.addEventListener('click', () => {
-    sideBar.classList.toggle('side-bar-active')
+    sideBar.classList.toggle('side-bar-active');
     // Toggle Nav
     // linksWrapper.classList.toggle('nav-active');
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = '';
       } else {
-        link.style.animation = `navLinkFade 0.3s ease forwards ${index /7 + .2}s`
+        link.style.animation = `navLinkFade 0.3s ease forwards ${
+          index / 7 + 0.2
+        }s`;
       }
     });
 
@@ -33,14 +34,14 @@ const navSlide = () => {
     //   }
     // });
     // Burger Animation
-    burger.classList.toggle('toggle-burger-inset')
-    burgerLines.classList.toggle('toggle-burger-lines')
+    burger.classList.toggle('toggle-burger-inset');
+    burgerLines.classList.toggle('toggle-burger-lines');
   });
   // linksWrapper.style.transition = 'none';
-}
+};
 
 const app = () => {
- navSlide();
-}
+  navSlide();
+};
 
 app();
